a837b26211b618e38c0520d5a4ba7676ac8a7d66
Naoki Ikeguchi <n_ikeguchi@yumemi.co.jp>
2020-12-17 15:04:01 +0900

    ➕ `composer require sensio/framework-extra-bundle symfony/twig-bundle`

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
